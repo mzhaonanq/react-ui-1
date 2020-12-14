0e8153ba2aad0ffb9f464c74684626db31a13604
mzhaonanq <1002604003@qq.com>
2020-12-14 17:04:49 +0800

    完成Icon的svg自动引入

--- src/Icon.tsx
+++ src/Icon.tsx
@@ -1,17 +1,19 @@
 import React from 'react';
-import  './left.svg'
 
-type Props={
+let importAll = (requireContext: __WebpackModuleApi.RequireContext) => requireContext.keys().forEach(requireContext);
+try {importAll(require.context('./', true, /\.svg$/));} catch (error) {console.log(error);}
+
+type Props = {
   name?: string
 }
-const Icon: React.FunctionComponent<Props> =(props)=>{
-  return(
+const Icon: React.FunctionComponent<Props> = (props) => {
+  return (
     <div>
       <svg>
         <use xlinkHref={`#${props.name}`}/>
       </svg>
     </div>
-  )
-}
+  );
+};
 
 export {Icon}
