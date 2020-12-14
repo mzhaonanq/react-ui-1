68b055efa717f96c6f013c80f38c8c0c709f314a
mzhaonanq <1002604003@qq.com>
2020-12-14 10:58:26 +0800

    完成webpack.dev和webpack.prod分开配置

---
+++ webpack.config.dev.js
@@ -0,0 +1,8 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+const base = require('./webpack.config')
+module.exports = Object.assign({}, base, {
+  mode: "development",
+  plugins: [
+    new HtmlWebpackPlugin({title: "React-UI", template: "./lib/index.html"}),
+  ],
+})
--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,5 @@
 const path = require('path')
-const HtmlWebpackPlugin = require('html-webpack-plugin')
 module.exports = {
-  mode: "development",
   entry: './lib/index.tsx',
   output: {
     path: path.resolve(__dirname, 'dist'),
@@ -19,21 +17,4 @@ module.exports = {
   resolve: {
     extensions: ['.tsx', '.ts', '.jsx', '.js'],
   },
-  plugins: [
-    new HtmlWebpackPlugin({title: "React-UI", template: "./lib/index.html"}),
-  ],
-  externals: {
-    "react": {
-      root: 'React',
-      commonjs2: 'react',
-      commonjs: 'react',
-      amd: 'react'
-    },
-    'react-dom': {
-      root: 'ReactDOM',
-      commonjs2: 'react-dom',
-      commonjs: 'react-dom',
-      amd: 'react-dom'
-    }
-  }
 }
